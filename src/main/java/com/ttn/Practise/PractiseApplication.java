72764abc06bc432bcff89039faccaaa5ab1af1ad
manvi <manvi.jain1@tothenew.com>
2025-02-26 15:46:29 +0530

    Added a repository and a gson dependency and used it in PractiseApplication.java

--- src/main/java/com/ttn/Practise/PractiseApplication.java
+++ src/main/java/com/ttn/Practise/PractiseApplication.java
@@ -2,13 +2,25 @@ package com.ttn.Practise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.google.gson.Gson;
 
+class Admin{
+	public String name;
+	public String password;
+
+	public Admin(String name, String password){
+		this.name = name;
+		this.password = password;
+	}
+
+}
 @SpringBootApplication
 public class PractiseApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(PractiseApplication.class, args);
+		Admin admin = new Admin("Manvi", "Manvi123@");
+		Gson gson = new Gson();
+		System.out.println(gson.toJson(admin));
 	}
 
 }
