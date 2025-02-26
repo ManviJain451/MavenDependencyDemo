6752b166c7ae8e447ac097d57484c07d38df6e36
manvi <manvi.jain1@tothenew.com>
2025-02-26 15:33:46 +0530

    Inital commit, no chnages

---
+++ src/test/java/com/ttn/Practise/PractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttn.Practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
